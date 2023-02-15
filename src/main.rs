25e1937dd8c230b30ec26bb9267f0a4d5cd8628f
Guerrero Ariel <ariel_guerrero1012@gmail.com>
2023-02-15 07:11:15 +0000

    chore: refactored mostly everything, blew up the diesel stuff, needs to be set back up since db conn is there

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder}; // no
+use actix_web::{ 
+    get, 
+    post, 
+    web, 
+    App, 
+    HttpResponse, 
+    HttpServer,
+    Responder,
+    Result,
+}; 
 
+use serde::{
+    Deserialize, 
+    Serialize
+};
+
+use std::env;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Data {
+    stuff: String,
+}
+
+// deserialize `Info` from request's body
+#[post("/submit")]
+async fn submit(info: web::Json<Data>) -> Result<String> {
+    Ok(format!("Welcome {}!", info.stuff))
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -8,27 +34,26 @@ async fn hello() -> impl Responder {
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+    HttpResponse::Ok().body(req_body) 
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-#[get("/")]
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn manual_hello() -> impl Responder { 
+    HttpResponse::Ok().body("Hey there!") 
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    if env::var("ENV_HOST").is_err() { env::set_var("ENV_HOST", "localhost") }
+    let env_host: String = env::var("ENV_HOST").expect("Host must be set");
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("0.0.0.0", 8080))?
+    .bind((env_host, 8080))?
     .run()
     .await
 }
